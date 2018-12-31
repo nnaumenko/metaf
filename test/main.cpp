ac1ab1c396047558db125a60755886487c5e3a79
nnaumenko <nikolai.naumenko@gmail.com>
2019-01-01 00:02:43 +0200

    Cosmetics: it's 2019 now!

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2018 Nick Naumenko (https://gitlab.com/nnaumenko)
+* Copyright (C) 2018-2019 Nick Naumenko (https://gitlab.com/nnaumenko)
 * All rights reserved.
 * This software may be modified and distributed under the terms
 * of the MIT license. See the LICENSE file for details.
